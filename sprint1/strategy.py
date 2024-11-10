e8b8e355a02f8373cc0c9431c272acfc993d8835
sir1215 <c26sr@dalton.org>
2024-11-09 21:22:06 -0500

    Starting MACD Calculation

--- sprint1/strategy.py
+++ sprint1/strategy.py
@@ -1,5 +1,4 @@
-from math import *
-from ta import *
+import ta
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,5 +6,13 @@ from datetime import datetime
 from datetime import timedelta
 import yfinance as yf
 #all imports
-price = yf.Ticker('AAPL').history(period='1y').reset_index()[['Date', 'Close']]
-plt.plot(price['Date'], price['Close'])
+ticker = "TSLA"
+#price = yf.Ticker('TSLA').history(period='1y').reset_index()[['Date', 'Close']]
+data = yf.download(ticker,start="2024-10-05",end="2024-11-05", interval="5m")
+data["MACD"] = ta.trend.MACD(data["Close"]).macd()
+data["MACD_Signal"] = ta.trend.MACD(data["Close"]).macd_signal()
+macd_data = data[["MACD","MACD_Signal","MACD_Hist"]].dropna()
+print(macd_data)
+#for x in range(0,len(macd_data))
+
+#plt.plot(price['Date'], price['Close'])
