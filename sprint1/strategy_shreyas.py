b2f87fecc709cabd69b4ab46c2f9f9b9aaa04dd6
sir1215 <c26sr@dalton.org>
2024-11-14 09:14:04 -0500

    Added position sizing

--- sprint1/strategy_shreyas.py
+++ sprint1/strategy_shreyas.py
@@ -1,80 +1,48 @@
 import ta
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import datetime
 import yfinance as yf
 import backtrader as bt
-import backtrader.feeds as btfeeds
 
 class Strategy(bt.Strategy):
     def __init__(self):
-        self.rsi = bt.indicators.RSI(self.data.close,period=14)
-        self.ema12 = bt.indicators.EMA(self.data.close,period=12)
-        self.ema26 = bt.indicators.EMA(self.data.close,period=26)
+        self.rsi = bt.indicators.RSI(self.data.close, period=14)
+        self.ema12 = bt.indicators.EMA(self.data.close, period=12)
+        self.ema26 = bt.indicators.EMA(self.data.close, period=26)
         self.macd = self.ema12 - self.ema26
-        self.adx = bt.indicators.AverageDirectionalIndex
+        self.bbands = bt.indicators.BollingerBands(self.data.close,period=20,devfactor=2.0)
+        self.adx = bt.indicators.AverageDirectionalMovementIndex(self.data)
+        self.atr = bt.indicators.AverageTrueRange(self.data,period=14)
+    def determine_position_size(self):
+        pass
+    # Add our function for determining position size
+
     def next(self):
-        self.log('Close, %.2f' % self.dataclose[0])
+        # position_size = self.determine_position_size()
+        self.debug(f'Close: {self.data.close[0]:.2f}, RSI: {self.rsi[0]:.2f}, MACD: {self.macd[0]:.2f}, ADX: {self.adx[0]:.2f}')
+        if self.rsi < 40 and self.macd > 0 and not self.position and self.adx > 25:
+            self.debug('Buy Signal')
+            self.buy() # Later self.buy(size = position.size)
+        elif self.rsi > 60 and self.macd < 0 and self.position and self.adx > 25:
+            self.debug('Sell Signal')
+            self.sell() # Later self.sell(size = position.size)
+        else:
+            pass
 
-        if self.rsi < 30 and self.macd > 0 and self.position != True and self.adx > 25:
-            self.buy()
-        elif self.rsi >70 and self.macd < 0 and self.position == True and self.adx > 25:
-            self.sell()
+    def debug(self, text):
+        print(text)
 
-def backtest(ticker):
+def backtest(stock):
     cerebro = bt.Cerebro()
     cerebro.addstrategy(Strategy)
     cerebro.broker.setcash(100000.0)
-    data = btfeeds.YahooFinanceCSVData(dataname='yfdata.csv',fromdate=datetime.datetime(2024, 10, 9), todate=datetime.datetime(2024, 11, 9))
+    price = yf.Ticker(stock).history(period="5d", interval="1m")
+    price.index = pd.to_datetime(price.index)
+    data = bt.feeds.PandasData(dataname=price)
     cerebro.adddata(data)
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
     cerebro.run()
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-backtest("AAPL")
 
-
-# def ema(data,length):
-#     return data.ewm(span=length).mean()
-
-# def get_macd(data):
-#     data['EMA_12'] = ema(data['Close'], 12)
-#     data['EMA_26'] = ema(data['Close'], 26)
-#     data['MACD'] = data['EMA_12'] - data['EMA_26']
-#     data = data.drop(columns=['EMA_12', 'EMA_26'])
-#     return data['MACD']
-
-# def get_rsi(data):
-#     changes = data['Close'].diff()
-#     avg_gains = (changes.where(changes > 0, 0)).rolling(window=14).mean()
-#     avg_losses = (abs(changes).where(changes < 0, 0)).rolling(window=14).mean()
-#     rs = avg_gains/avg_losses
-#     rsi = 100 - (100 / (1 + rs))
-#     return rsi
-
-
-# def calculate_indicators_5min(ticker):
-#     data = yf.download(ticker, start="2024-10-05", end="2024-11-05", interval="5m")
-
-#     data['MACD'] = get_macd(data)
-#     data['RSI'] = get_rsi(data)
-
-#     data = data.dropna()
-
-#     signals = []
-#     for i in range(len(data)):
-#         macd = data['MACD'].iloc[i]
-#         rsi = data['RSI'].iloc[i]
-
-#         if rsi < 30 and macd > 0:
-#             signals.append((data.index[i], "Buy",rsi,macd))
-#         elif rsi > 70 and macd < 0:
-#             signals.append((data.index[i], "Sell",rsi,macd))
-#     for signal in signals:
-#         print(f"{signal[0]} - {signal[1]} because RSI = {signal[2]} and MACD = {signal[3]}")
-    
-#     return signals
-
-# ticker = "TSLA"
-# calculate_indicators_5min(ticker)
-
+stock = "TSLA"
+backtest(stock)
