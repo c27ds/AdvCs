f1cc62f44d805dd41d2be74a4c9f14aceeb0ce58
sir1215 <c26sr@dalton.org>
2024-11-26 12:08:21 -0500

    Backtest working

--- sprint1/strategy3.py
+++ sprint1/strategy3.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-import matplotlib.pyplot as plt
-
+from matplotlib import pyplot as plt
 import backtrader as bt
 import backtrader.feeds as btfeeds
 
@@ -16,42 +15,46 @@ class Strategy(bt.Strategy):
         self.bbands = bt.indicators.BollingerBands(self.data.close, period=56, devfactor=2.0)
         self.adx = bt.indicators.AverageDirectionalMovementIndex(self.data)
         self.atr = bt.indicators.AverageTrueRange(self.data, period=14)
-        self.bbdiff = (self.bbands.lines.top - self.bbands.lines.bot)*100/self.data.close
+        self.bbdiff = (self.bbands.lines.top - self.bbands.lines.bot) * 100 / self.data.close[0]
+        self.size = 10
         self.b = 0
         self.s = 0
-        self.h = 0
-        
-    def determine_position_size(self):
-        pass
 
     def next(self):
-        self.debug(f'Close: {self.data.close[0]:.2f}, RSI: {self.rsi[0]:.2f}, MACD: {self.macd[0]:.2f}, ADX: {self.adx[0]:.2f}, BBDIFF: {self.bbdiff[0]:.2f}')
-        # if self.position:
-        #     if self.data.close[0] < self.stop_price:
-        #         self.debug(f'Stop Loss at {self.data.close[0]:.2f}, Selling Position')
-        #         self.close()
-        #         self.stop_price = None
-        if self.rsi[0] > 70 and self.macd_signal[0] > 0.1 and self.adx[0] > 27.5 and (self.bbdiff[0] < 5 and self.bbdiff[0] > 2):
-            self.b += 1
-            self.debug(f'Buy Signal # {self.b}')
-            self.buy()
-            self.stop_price = self.data.close[0] * 0.98
-            
-        elif self.rsi[0] < 30 and self.macd_signal[0] < -0.1 and self.adx[0] > 27.5 and self.position and (self.bbdiff[0] < 5 and self.bbdiff[0] > 2):
-            self.s += 1
-            self.debug(f'Sell Signal # {self.s}')
-            self.sell()
+        self.debug(f'Close: {self.data.close[0]:.2f}, RSI: {self.rsi[0]:.2f}, MACD: {self.macd[0]:.2f}, ADX: {self.adx[0]:.2f}, BBandsDIFF: {self.bbdiff[0]:.2f}')
+        if (self.rsi[0] > 70 and self.macd_signal[0] > 0.1 and self.adx[0] > 27.5 and (self.bbdiff[0] < 5 and self.bbdiff[0] > 2)):
+            if self.position.size < 0:
+                self.close()
+                self.debug("Closing Short Position")
+                cash = self.broker.getvalue()
+                print(cash)
+            else:
+                self.b += 1
+                self.debug(f'Buy Signal #{self.b}')
+                self.buy(size=10)
+                cash = self.broker.getvalue()
+                print(cash)
+
+        elif (self.rsi[0] < 30 and self.macd_signal[0] < -0.1 and self.adx[0] > 27.5 and (self.bbdiff[0] < 5 and self.bbdiff[0] > 2)):
+            if self.position.size > 0:
+                self.close()
+                self.debug("Closing Long Position")
+                cash = self.broker.getvalue()
+                print(cash)
+            else:
+                self.s += 1
+                self.debug(f'Short Signal #{self.s}')
+                self.sell(size=10)
+                cash = self.broker.getvalue()
+                print(cash)
         
-        elif self.rsi[0] < 30 and self.macd_signal[0] < -0.1 and self.adx[0] > 27.5 and not self.position and (self.bbdiff[0] < 5 and self.bbdiff[0] > 2):
-            self.h += 1
-            self.debug(f'Short # {self.h}')
-            self.short()
-            
-        else:
-            pass
 
     def debug(self, text):
         print(text)
+        
+    def totaltrades(self):
+        return self.b + self.s,
+
 class PerformanceAnalyzer(bt.Analyzer):
     def __init__(self):
         self.returns = []
@@ -64,6 +67,7 @@ class PerformanceAnalyzer(bt.Analyzer):
         if trade.isclosed:
             pnl = trade.pnl
             self.returns.append(pnl)
+            print(pnl)
             if pnl > 0:
                 self.win_trades += 1
                 self.total_profit += pnl
@@ -77,14 +81,25 @@ class PerformanceAnalyzer(bt.Analyzer):
         profit_factor = (self.total_profit / self.total_loss) if self.total_loss > 0 else 0
         sharpe_ratio = self.strategy.analyzers.sharpe.get_analysis().get("sharperatio")
         max_drawdown = (self.strategy.analyzers.drawdown.get_analysis().get("max").get("drawdown"))*100
-
         return {
             'Total Trades': total_trades,
             'Win Rate (%)': win_rate,
             'Profit Factor': profit_factor,
+            'Total Profit': self.total_profit,
+            'Total Loss': self.total_loss,
             'Sharpe Ratio': sharpe_ratio,
-            'Max Drawdown': max_drawdown,
+            'Max Drawdown': max_drawdown
         }
+    
+    def notify_order(self, order):
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                print(f"BUY EXECUTED: Price={order.executed.price:.2f}, Size={order.executed.size}")
+            elif order.issell():
+                print(f"SELL EXECUTED: Price={order.executed.price:.2f}, Size={order.executed.size}")
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            print("Order Canceled/Margin/Rejected")
+
 
 def backtest(stock):
     cerebro = bt.Cerebro()
@@ -101,6 +116,7 @@ def backtest(stock):
     results = cerebro.run()
     print(f'Final Portfolio Value: {cerebro.broker.getvalue()}')
     performance = results[0].analyzers.performance.get_analysis()
+    print(performance)
     print("Analysis:")
     for i in performance:
         print(f"{i}: {performance[i]}")
