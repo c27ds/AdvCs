b8dfae5605637214480bc97eaadc6a3947e92a8b
sir1215 <c26sr@dalton.org>
2025-01-17 14:11:58 -0500

    final debug

--- sprint1/Backtesting/multistock.py
+++ sprint1/Backtesting/multistock.py
@@ -4,6 +4,9 @@ import rnn
 import lstm
 stocks = ["XLK","QQQ","TSLA","MSFT","AAPL"]
 def backtest_all(stocks):
+    lstm_results = {"Total Return":return_total, "Max Drawdown":strat.analyzers.drawdown.get_analysis()['max']['drawdown'], "Sharpe Ratio":sharpe_ratio if sharpe_ratio is not None else 'N/A'}
+    rnn_results
+    knn_results
     for i in stocks:
         lstm_results = lstm.backtest(i)
         rnn_results = rnn.backtest(i)
