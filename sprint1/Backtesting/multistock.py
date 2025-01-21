09930e121e16a087e00c2660dd62358df02f50a4
sir1215 <c26sr@dalton.org>
2025-01-21 12:13:30 -0500

    End redundancy

--- sprint1/Backtesting/multistock.py
+++ sprint1/Backtesting/multistock.py
@@ -4,11 +4,21 @@ import sprint1.Models.rnn as rnn
 import sprint1.Models.lstm as lstm
 stocks = ["XLK","QQQ","TSLA","MSFT","AAPL"]
 def backtest_all(stocks):
-    lstm_results = {"Total Return":return_total, "Max Drawdown":strat.analyzers.drawdown.get_analysis()['max']['drawdown'], "Sharpe Ratio":sharpe_ratio if sharpe_ratio is not None else 'N/A'}
-    rnn_results
-    knn_results
+    lstm_results = {"Total Return":[], "Max Drawdown":[], "Sharpe Ratio":[]}
+    rnn_results = {"Total Return":[], "Max Drawdown":[], "Sharpe Ratio":[]}
+    knn_results = {"Total Return":[], "Max Drawdown":[], "Sharpe Ratio":[]}
     for i in stocks:
-        lstm_results = lstm.backtest(i)
-        rnn_results = rnn.backtest(i)
-        knn_results = knn.backtest(i)
+        lstm_res = lstm.backtest(i)
+        rnn_res = rnn.backtest(i)
+        knn_res = knn.backtest(i)
+        lstm_results["Max Drawdown"].append(lstm_res["Max Drawdown"])
+        lstm_results["Total Return"].append(lstm_res["Total Return"])
+        lstm_results["Sharpe Ratio"].append(lstm_res["Sharpe Ratio"])
+        rnn_results["Max Drawdown"].append(rnn_res["Max Drawdown"])
+        rnn_results["Total Return"].append(rnn_res["Total Return"])
+        rnn_results["Sharpe Ratio"].append(rnn_res["Sharpe Ratio"])
+        knn_results["Max Drawdown"].append(knn_res["Max Drawdown"])
+        knn_results["Total Return"].append(knn_res["Total Return"])
+        knn_results["Sharpe Ratio"].append(knn_res["Sharpe Ratio"])
+        
 
